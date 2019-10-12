c63f78a9d5a197957fb62e278e1a24901da303ef
szkabaroli <szkabaroli@gmail.com>
2019-10-12 09:42:38 +0200

    Initial setup :wrench:

---
+++ src/react/App.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import './App.scss'
+
+const App = () => <p>Hello React</p>
+
+export default App
---
+++ src/vue/App.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+	p Hello Vue
+</template>
+
+<script>
+	import Vue from "vue"
+
+	export default {
+		
+	}
+</script>
+
+<style lang="scss" scoped>
+	p {
+		color: green;
+	}
+</style>
